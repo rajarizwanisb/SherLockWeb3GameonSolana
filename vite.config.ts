ca0aa44cdd1b4535137a47b365e105cf7046f27a
Your Namerajarizwanisb <your-email@example.comrajastar2018@gmail.com>
2025-08-25 12:49:40 +0500

    Final attempt with new index.html and vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  // Use a relative base path to ensure all assets are loaded correctly
-  base: './',
-
   plugins: [react()],
-
-  // Configure the build process to output to the correct directory
-  build: {
-    outDir: 'dist',
-    assetsDir: '', // Use an empty string to keep assets in the root of the 'dist' directory
-  },
-
-  // This ensures the paths are correct for the deployment
-  publicDir: 'public',
-
-  resolve: {
-    alias: {
-      src: path.resolve(__dirname, './src'),
-    },
-  },
-});
+  // This ensures assets are loaded correctly on GitHub Pages
+  base: '/SherLockWeb3GameonSolana/'
+})
