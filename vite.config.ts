9a3bc1a4119c2a292835ef1838b5bf23d5afb356
Your Namerajarizwanisb <your-email@example.comrajastar2018@gmail.com>
2025-08-26 20:21:30 +0500

    Final final final final fix for PostCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This is the key fix that ensures assets are loaded correctly on GitHub Pages
   base: './',
 });
